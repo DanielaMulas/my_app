edbaded99ad03d797c17f4ec4a0f73e7d626afe3
Daniela Mulas <danielamulas96@hotmail.com>
2023-05-13 17:13:50 +0200

    Added BottomNavigationBar

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -22,17 +22,43 @@ class HomePage extends StatelessWidget{
             Text('Welcome back *Username*!',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0)),
           ],),]
         ),
-        // Cose da aggiungere
+              // Cose da aggiungere
       ),
-         
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        //Changing color to icons when selected 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////Qua è da risolvere
+        selectedItemColor: Colors.grey, 
+        selectedLabelStyle: const TextStyle(color: Colors.black, fontSize: 14),
+        unselectedLabelStyle: const TextStyle(color: Colors.black, fontSize: 14),
+        backgroundColor: Colors.pink,
+        items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white),
+            label: "Home",       
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event_available, color: Colors.white),
+            label: 'Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart, color: Colors.white),
+            label: 'Stats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.white),
+            label: 'Profile',
+          ),
+        ],
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-             ListTile(
+            ListTile(
               title: const Text('Menu', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
               leading: Icon(Icons.menu),
-              
             ),
             ListTile(
               title: const Text('Profile'),
