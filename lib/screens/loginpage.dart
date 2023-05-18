b2fd09c1572f269741d8ca50cfc6965d2f8f3021
Christian Dragonetti <christian.dragonetti@studenti.unipd.it>
2023-05-18 18:53:14 +0200

    creato aboutpage

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:my_app/screens/homepage.dart';
+import 'package:my_app/screens/aboutpage.dart';
 import 'package:flutter_login/flutter_login.dart';
 
 class LoginPage extends StatelessWidget{
   const LoginPage({Key? key}) : super(key:key);
 
   static const routeDisplayName = 'LoginPage';
-
 
 
   @override
@@ -26,16 +26,27 @@ class LoginPage extends StatelessWidget{
               child: Text('To the HomePage'),
               onPressed: () => _toHomePage(context),
             ),
+            ElevatedButton(
+              child: Text('To the AboutPage'),
+              onPressed: () => _toAboutPage(context),
+            ),
           ],
         ),
       ), 
 
     );
   }
+
+
   void _toHomePage(BuildContext context){
     //Pop
     Navigator.pop(context);
     //Push HomePage
     Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
   }
+
+  void _toAboutPage(BuildContext context){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => AboutPage()));
+  }
+
 }
